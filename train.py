6a63e6333e2f606e0813a9e2a2b10f6016388efb
chu thomas <chu_liang_han@hotmail.com>
2023-03-21 18:01:02 +0800

    3/21 update

--- train.py
+++ train.py
@@ -1,7 +1,9 @@
 from datasets import load_dataset
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, AutoModelForCausalLM
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
+from transformers import GPT2Tokenizer, TFGPT2LMHeadModel, GPT2Model
 from transformers import Trainer, TrainingArguments
+from transformers import DefaultDataCollator
+
 import torch
 
 def main():
@@ -10,47 +12,55 @@ def main():
     train_test_dataset = (load_dataset('json', data_files='source/train.json',split='train')).train_test_split(test_size=0.2)
     print(train_test_dataset['train'][0])
 
-    model_name = "distilgpt2"
+    # model_name = "distilgpt2"
+    # model_name = "gpt2"
+    model_name = "bert-base-cased"
+    # model_name = "distilbert-base-uncased"
     device = "cuda" if torch.cuda.is_available() else "cpu"
+
     # model = TFGPT2LMHeadModel.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    # tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.pad_token = tokenizer.eos_token
 
-    def case_name_tokenize(sets):
-        return tokenizer(sets["case_name"], sets["case_script"],padding="max_length", max_length=256, truncation=True)
-    case_name_ids = train_test_dataset.map(case_name_tokenize, batched=True)
+    if tokenizer.pad_token is None:
+        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 
-    # def case_script_tokenize(sets):
-    #    return tokenizer(sets["case_script"], padding="max_length", truncation=True)
-    # case_script_ids = train_test_dataset.map(case_script_tokenize, batched=True)
+    def case_tokenize(sets):
+        return tokenizer(sets["case_name"], sets["case_script"],padding="max_length", max_length=512,
+                         truncation=True)
+    train_ids = train_test_dataset['train'].map(case_tokenize, batched=True)
+    test_ids = train_test_dataset['test'].map(case_tokenize, batched=True)
+
+    # print(train_ids['input_ids'])
+
+    data_collator = DefaultDataCollator()
+
     training_args = TrainingArguments(
         output_dir='./results',
         overwrite_output_dir=True,
         num_train_epochs=1,
+        learning_rate=2e-5,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
-        save_steps=100,
+        # save_steps=100,
         save_total_limit=2,
+        weight_decay=0.01,
         # prediction_loss_only=True,
-        # evaluation_strategy='steps',
+        evaluation_strategy='no',
     )
-
-    # training_args = TrainingArguments(output_dir="test_trainer", evaluation_strategy="epoch")
 
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=case_name_ids["train"],
-        eval_dataset=case_name_ids["test"],
+        train_dataset=train_ids,
+        eval_dataset=test_ids,
         tokenizer=tokenizer,
+        data_collator=data_collator
     )
     trainer.train()
-
-    # model.compile(optimizer='adam', loss=model.compute_loss)
-    # model.fit(case_name_ids, case_script_ids, epochs=5)
-
-
+    trainer.save_model(./acts_model)
 
 if __name__ == '__main__':
     main()
