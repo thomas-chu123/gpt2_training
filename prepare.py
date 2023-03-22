bdd10a9a8fe8c77b4a78f4cd54e9e0cfa5728e49
chu thomas <chu_liang_han@hotmail.com>
2023-03-22 19:34:20 +0800

    3/22 update

--- prepare.py
+++ prepare.py
@@ -21,16 +21,18 @@ def main():
                 count = count + 1
                 print(count, script_string)
                 script_string = {'case_name': '', 'case_script': ''}
-                script_string["case_name"] = text_line.replace('# ', '')
+                script_string["case_name"] = text_line.rstrip().replace('# ', '')
             # for new start test item
             elif start_flag == False and end_flag == False:
                 start_flag = True
                 end_flag = False
                 script_string = {'case_name': '', 'case_script': ''}
-                script_string["case_name"] = text_line.replace('# ', '')
+                script_string["case_name"] = text_line.rstrip().replace('# ', '')
+        elif text_line=='':
+            continue
         else:
             if start_flag==True and end_flag==False:
-                script_string["case_script"] = script_string["case_script"] + text_line + "\r\n"
+                script_string["case_script"] = script_string["case_script"] + text_line.rstrip().lstrip() + ' [SEP] '
 
     with open("source/train.json","w",encoding="utf-8") as f:
         f.write(json.dumps(script_list))
--- train.py
+++ train.py
@@ -12,6 +12,15 @@ def main():
     train_test_dataset = (load_dataset('json', data_files='source/train.json',split='train')).train_test_split(test_size=0.2)
     print(train_test_dataset['train'][0])
 
+    # i=0
+    # for one_set in train_test_dataset['train']['case_script']:
+    #    script =''
+    #    for item in one_set:
+    #        script = ''.join(item) + '[sep]'
+    #    train_test_dataset['train']['case_script'][i] = script
+    #    i = i + 1
+
+    print(train_test_dataset['train'][0])
     # model_name = "distilgpt2"
     # model_name = "gpt2"
     model_name = "bert-base-cased"
@@ -22,14 +31,13 @@ def main():
     # tokenizer = GPT2Tokenizer.from_pretrained(model_name)
     model = AutoModelForQuestionAnswering.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.pad_token = tokenizer.eos_token
-
     if tokenizer.pad_token is None:
         tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    if tokenizer.eos_token is None:
+        tokenizer.add_special_tokens({'eos_token': '[SEP]'})
 
     def case_tokenize(sets):
-        return tokenizer(sets["case_name"], sets["case_script"],padding="max_length", max_length=512,
-                         truncation=True)
+        return tokenizer(sets["case_name"], sets["case_script"], max_length=256, padding=True, truncation=True)
     train_ids = train_test_dataset['train'].map(case_tokenize, batched=True)
     test_ids = train_test_dataset['test'].map(case_tokenize, batched=True)
 
@@ -44,11 +52,11 @@ def main():
         learning_rate=2e-5,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
-        # save_steps=100,
+        save_steps=100,
         save_total_limit=2,
         weight_decay=0.01,
-        # prediction_loss_only=True,
-        evaluation_strategy='no',
+        prediction_loss_only=True,
+        evaluation_strategy='epoch',
     )
 
     trainer = Trainer(
