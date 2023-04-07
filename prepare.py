a3b3aa24a14e64356a7abdf95ed999d6eef33a4c
chu thomas <chu_liang_han@hotmail.com>
2023-04-07 18:10:30 +0800

    4/7 update

--- prepare.py
+++ prepare.py
@@ -10,7 +10,7 @@ def main():
     with open("source/Function_V6.2.dlf","r",encoding="utf-8") as f:
         text = f.read()
     text_list = text.split("\n")
-    script_string = {'question': '', 'context': ''}
+    script_string = {'question': '', 'context': '', 'data': ''}
     for text_line in text_list:
         if text_line.startswith("# "):
             # for next new script line
@@ -20,7 +20,7 @@ def main():
                 script_list.append(script_string)
                 count = count + 1
                 print(count, script_string)
-                script_string = {'question': '', 'context': ''}
+                script_string = {'question': '', 'context': '', 'data': ''}
                 script_string["question"] = text_line.rstrip().replace('# ', '')
             # for new start test item
             elif start_flag == False and end_flag == False:
@@ -33,6 +33,7 @@ def main():
         else:
             if start_flag==True and end_flag==False:
                 script_string["context"] = script_string["context"] + text_line.rstrip().lstrip() + '\n'
+                script_string["data"] = script_string["question"] + "\n" + script_string["context"]
 
     with open("source/train.json","w",encoding="utf-8") as f:
         f.write(json.dumps(script_list))
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForQuestionAnswering, AutoModelForCausalLM, \
+    DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 from transformers import DefaultDataCollator
 import torch
@@ -7,7 +8,7 @@ import torch
 def main():
     torch.cuda.empty_cache()
     train_test_dataset = (load_dataset('json', data_files='source/train.json',split='train')).train_test_split(test_size=0.2)
-    print(train_test_dataset['train'][0])
+    # print(train_test_dataset['train'][0])
 
     model_name = "distilgpt2"
     # model_name = "bert-base-uncased"
@@ -15,37 +16,48 @@ def main():
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    if tokenizer.pad_token is None:
-        tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-    if tokenizer.eos_token is None:
-        tokenizer.add_special_tokens({'eos_token': '[SEP]'})
+    tokenizer.pad_token = tokenizer.eos_token
+    # if tokenizer.pad_token is None:
+    #    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+    # if tokenizer.eos_token is None:
+    #    tokenizer.add_special_tokens({'eos_token': '[SEP]'})
 
     def case_tokenize(sets):
-        return tokenizer(sets["context"],
+        return tokenizer(sets["data"],
                          max_length=128,
-                         padding=True,
+                         padding='max_length',
                          truncation=True,
                          # return_overflowing_tokens=True,
                          # return_offsets_mapping=True,
                          )
-    train_ids = train_test_dataset['train'].map(case_tokenize, batched=True)
-    test_ids = train_test_dataset['test'].map(case_tokenize, batched=True)
+    train_ids = train_test_dataset['train'].map(case_tokenize, batched=True, remove_columns=['data','context', 'question'])
+    test_ids = train_test_dataset['test'].map(case_tokenize, batched=True, remove_columns=['data','context', 'question'])
 
-    print(tokenizer.decode(train_ids["input_ids"][0]))
+    def add_labels(labels):
+        labels["labels"] = labels["input_ids"].copy()
+        return labels
+
+    train_ids = train_ids.map(add_labels, batched=True, num_proc=4)
+    test_ids = test_ids.map(add_labels, batched=True, num_proc=4)
+
+    print(tokenizer.decode(train_ids["labels"][0]))
+
+
     # print(train_ids['input_ids'][0])
     # print(train_ids.keys())
 
-    data_collator = DefaultDataCollator()
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+    # data_collator = DefaultDataCollator()
 
     training_args = TrainingArguments(
         output_dir='./results',
         overwrite_output_dir=True,
-        num_train_epochs=1,
+        # num_train_epochs=1,
         learning_rate=2e-5,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
-        save_steps=100,
-        save_total_limit=2,
+        # per_device_train_batch_size=16,
+        # per_device_eval_batch_size=16,
+        # save_steps=100,
+        # save_total_limit=2,
         weight_decay=0.1,
         # prediction_loss_only=True,
         evaluation_strategy='epoch',
@@ -56,7 +68,7 @@ def main():
         args=training_args,
         train_dataset=train_ids,
         eval_dataset=test_ids,
-        tokenizer=tokenizer,
+        # tokenizer=tokenizer,
         data_collator=data_collator
     )
     trainer.train()
