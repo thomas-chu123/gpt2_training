1b1c70ed5d63d88563b68c546a6d239089ced7c5
securitymy <chu_liang_han@hotmail.com>
2023-04-07 21:53:23 +0800

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+prompt = "login gui and generate packet and ping internet"
+default_tokenizer = "distilgpt2"
+# custom_data = "E:\\Python_Program\\gpt2_training\\acts_model"
+custom_model = "acts_model"
+
+tokenizer = AutoTokenizer.from_pretrained(custom_model)
+inputs = tokenizer(prompt, return_tensors="pt")
+
+model = AutoModelForCausalLM.from_pretrained(custom_model)
+outputs = model.generate(**inputs, max_length=256, do_sample=True, top_k=50, top_p=0.95, temperature=0.7, num_return_sequences=1)
+eval = model.eval()
+text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+for line in text:
+    print(line)
+
+print(eval)
